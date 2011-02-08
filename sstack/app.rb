b81b704ba0a3a0610de93238ad4728257e923d05
Francesco Canessa <makevoid@gmail.com>
2011-02-08 12:54:12 +0100

    added buttons in css + sinatra classy setup

--- sstack/app.rb
+++ sstack/app.rb
@@ -1,13 +1,25 @@
-path = File.expand_path "../", __FILE__
+require 'haml'
+require 'sass'
 require 'sinatra'
+enable :sessions
 
-require "#{path}/config/env"
+path = File.expand_path "../", __FILE__
+APP_PATH = path
+
+class App < Sinatra::Base
+  require "#{APP_PATH}/config/env"
 
 
-get "/" do
-  haml :index
-end
+  def not_found(object=nil)
+    halt 404, "404 - Page Not Found"
+  end
 
-get '/css/main.css' do
-  sass :main
+  get "/" do
+    haml :index
+  end
+
+  get '/css/main.css' do
+    sass :main
+  end
+  
 end
--- sstack/config.ru
+++ sstack/config.ru
@@ -2,4 +2,4 @@ path = File.expand_path "../", __FILE__
 
 
 require "#{path}/app"
-run Sinatra::Application
+run App
