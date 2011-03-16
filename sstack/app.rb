14e8ad23771099abfe74e3eba5fb4339dad47a44
Francesco Canessa <makevoid@gmail.com>
2011-03-16 01:31:20 +0100

    fixed sinatra stack and added content_for

--- sstack/app.rb
+++ sstack/app.rb
@@ -8,7 +8,14 @@ APP_PATH = path
 
 class App < Sinatra::Base
   require "#{APP_PATH}/config/env"
-
+  
+  set :haml, { :format => :html5 }
+  require 'rack-flash'
+  enable :sessions
+  use Rack::Flash
+  require 'sinatra/content_for'
+  helpers Sinatra::ContentFor
+  set :method_override, true
 
   def not_found(object=nil)
     halt 404, "404 - Page Not Found"
